c23acbb8fcf6801ca9f224791a343ddb45f4ad76
Kavya-thakur <thakurkavya471@gmail.com>
2026-01-05 13:56:39 +0530

    fixed the server.js file for webhook

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,17 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
 import createCashfreeOrder from "./routes/createCashfreeOrder.js";
 import cashfreeWebhook from "./routes/cashfreeWebhook.js";
 import checkPaymentStatus from "./routes/checkPaymentStatus.js";
+
 dotenv.config();
-console.log("CF CLIENT:", process.env.CASHFREE_CLIENT_ID);
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json({ limit: "1mb", type: "*/*" })); // 👈 important
 
-// 👉 use the separate route file
 app.use("/api/createCashfreeOrder", createCashfreeOrder);
 app.use("/api/cashfreeWebhook", cashfreeWebhook);
 app.use("/api/checkPaymentStatus", checkPaymentStatus);
