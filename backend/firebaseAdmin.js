08825af56fa1674f019c5259562e32b46df2b146
Kavya-thakur <thakurkavya471@gmail.com>
2026-01-05 14:10:27 +0530

    added the credential of firebase admin

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,8 +1,10 @@
 import admin from "firebase-admin";
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
