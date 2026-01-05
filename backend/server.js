df68a8a1f2154ab2d83412ab6efab92d8c2062d1
Kavya-thakur <thakurkavya471@gmail.com>
2026-01-05 16:21:49 +0530

    removing the unused vars

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,5 @@ app.use("/api/createCashfreeOrder", createCashfreeOrder);
 app.use("/api/cashfreeWebhook", cashfreeWebhook);
 app.use("/api/checkPaymentStatus", checkPaymentStatus);
 app.use("/api/products", products);
-// const PORT = process.env.PORT || 4000;
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
